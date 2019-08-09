504c0de35a27ca7b0fd6d4f5ea44668b7e0af7f4
lufeng <837091544@qq.com>
2019-08-09 08:21:16 +0800

    Update tomato.py
    更改为TK实现，弹出对话框，不再使用图片。

--- tomato.py
+++ tomato.py
@@ -1,60 +1,58 @@
 #!/usr/bin/python
-
-# 原作者：https://blog.csdn.net/qq_38190111/article/details/89044299 
-
-from PIL import Image, ImageTk
+#encoding:utf-8
 import tkinter as tk
+import tkinter.messagebox
 import time
+import sys
 
-def dialog():
-    window = tk.Tk()
-    window.title('休息一下吧!')
-    window.geometry('1200x900')
-    global img_png
-    Img = Image.open('./dialog.jpg')
-    img_png = ImageTk.PhotoImage(Img)
-    label_Img = tk.Label(window, image=img_png)
-    label_Img.pack()
-    window.mainloop()
+def dialog(studyTime, interval):
+    studyTime = str(studyTime)
+    interval  = str(interval)
+    root = tk.Tk()
+    root.withdraw()
+    confirm = tk.messagebox.showinfo(title = '番茄时钟提示', message = studyTime + '分钟过去了，休息' + interval + '分钟一下吧！')
+    if (confirm == "ok"):
+        root.destroy()	
 
 def study():
-    print('''             __                      __            __                     
-            /  |                    /  |          /  |                    
-  _______  _$$ |_    __    __   ____$$ | __    __ $$/  _______    ______  
- /       |/ $$   |  /  |  /  | /    $$ |/  |  /  |/  |/       \  /      \ 
-/$$$$$$$/ $$$$$$/   $$ |  $$ |/$$$$$$$ |$$ |  $$ |$$ |$$$$$$$  |/$$$$$$  |
-$$      \   $$ | __ $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |
- $$$$$$  |  $$ |/  |$$ \__$$ |$$ \__$$ |$$ \__$$ |$$ |$$ |  $$ |$$ \__$$ |
-/     $$/   $$  $$/ $$    $$/ $$    $$ |$$    $$ |$$ |$$ |  $$ |$$    $$ |
-$$$$$$$/     $$$$/   $$$$$$/   $$$$$$$/  $$$$$$$ |$$/ $$/   $$/  $$$$$$$ |
-                                        /  \__$$ |              /  \__$$ |
-                                        $$    $$/               $$    $$/ 
-                                         $$$$$$/                 $$$$$$/ ''')
+    print(''' 
+                   | |  (_)             
+__      _____  _ __| | ___ _ __   __ _  
+\ \ /\ / / _ \| '__| |/ / | '_ \ / _` | 
+ \ V  V / (_) | |  |   <| | | | | (_| | 
+  \_/\_/ \___/|_|  |_|\_\_|_| |_|\__, | 
+                                  __/ | 
+                                 |___/  
+ ''')
 def rest():
-    print('''                                 __     
-                                /  |    
-  ______    ______    _______  _$$ |_   
- /      \  /      \  /       |/ $$   |  
-/$$$$$$  |/$$$$$$  |/$$$$$$$/ $$$$$$/   
-$$ |  $$/ $$    $$ |$$      \   $$ | __ 
-$$ |      $$$$$$$$/  $$$$$$  |  $$ |/  |
-$$ |      $$       |/     $$/   $$  $$/ 
-$$/        $$$$$$$/ $$$$$$$/     $$$$/  ''')
+    print(''' 
+               | |  
+  _ __ ___  ___| |_ 
+ | '__/ _ \/ __| __|
+ | | |  __/\__ \ |_ 
+ |_|  \___||___/\__|
+ 
+ ''')
  
 #按分钟计时
 def run(studyTime,interval):
+    if len(sys.argv) == 3:
+        studyTime = float(sys.argv[1])
+        interval  = float(sys.argv[2])
     while True:
         try:
             study()
             time.sleep(studyTime*60) #就是睡指定时间
-            dialog()
-            print("学习暂停,进入5分钟休息时间")
+            dialog(studyTime,interval)
+            print("学习暂停,进入休息时间")
             rest()
             time.sleep(interval*60)#回到循环开头
         except Exception as e:
             print(e)
             
 if __name__ == "__main__":
+    for i in sys.argv:
+        print(i)
     studyTime = 25
     interval = 5
     run(studyTime,interval)
