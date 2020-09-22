37f95a2a01cdc3a08feff792e651b2a5cc2afd84
lufeng <lufeng.work@qq.com>
2020-09-22 08:56:28 +0800

    Update tomato.py

--- tomato.py
+++ tomato.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 
 # 原作者：https://blog.csdn.net/qq_38190111/article/details/89044299
@@ -57,44 +56,6 @@ def rest():
 
     ''')
 
-=======
-#!/usr/bin/python
-#encoding:utf-8
-import tkinter as tk
-import tkinter.messagebox
-import time
-import sys
-
-def dialog(studyTime, interval):
-    studyTime = str(studyTime)
-    interval  = str(interval)
-    root = tk.Tk()
-    root.withdraw()
-    confirm = tk.messagebox.showinfo(title = '番茄时钟提示', message = studyTime + '分钟过去了，休息' + interval + '分钟一下吧！')
-    if (confirm == "ok"):
-        root.destroy()	
-
-def study():
-    print(''' 
-                   | |  (_)             
-__      _____  _ __| | ___ _ __   __ _  
-\ \ /\ / / _ \| '__| |/ / | '_ \ / _` | 
- \ V  V / (_) | |  |   <| | | | | (_| | 
-  \_/\_/ \___/|_|  |_|\_\_|_| |_|\__, | 
-                                  __/ | 
-                                 |___/  
- ''')
-def rest():
-    print(''' 
-               | |  
-  _ __ ___  ___| |_ 
- | '__/ _ \/ __| __|
- | | |  __/\__ \ |_ 
- |_|  \___||___/\__|
- 
- ''')
- 
->>>>>>> 6057d1cd499376b1ef1d4b808e8c455bffb27991
 #按分钟计时
 def run(studyTime,interval):
     if len(sys.argv) == 3:
@@ -112,12 +73,6 @@ def run(studyTime,interval):
             print(e)
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     studyTime = 45
-=======
-    for i in sys.argv:
-        print(i)
-    studyTime = 25
->>>>>>> 6057d1cd499376b1ef1d4b808e8c455bffb27991
     interval = 5
     run(studyTime,interval)
